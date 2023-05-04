280b89d675c6f577ad6c4fec7c6a2fc60dad9c17
Simon <chmoulik770770@hotmail.com>
2023-05-04 21:40:36 +0200

    ajout fichier SQL

---
+++ src/controllers/ProduitController.php
@@ -0,0 +1,18 @@
+<?php
+
+class ProduitController
+
+{
+    public static function produits()
+    {
+        /*  afficher les bureaux:
+        select * Frome article WHERE categorie le faire dans la page view - foreach.
+
+        entré dans un article precis 
+
+        */
+
+
+        include VIEWS . "./new_user/produits.php";
+    }
+}
---
+++ views/new_user/produits.php
@@ -0,0 +1,27 @@
+<?php
+
+require(VIEWS . 'inc/head.php');
+require(VIEWS . 'inc/header.php');
+
+?>
+
+<h1>Categorie</h1>
+<nav>
+    <ul>
+        <li> <a href="<?= BASE_PATH ?>/produits"><img src="../../assets/img/bureau.png"></a>Bureaux</li>
+        <li> <a href="<?= BASE_PATH ?>/produits"><img src="../../assets/img/chaises.png"></a>Chaises</li>
+        <li> <a href="<?= BASE_PATH ?>/produits"><img src="../../assets/img/accessoirs.jpeg"></a>Accessoirs</li>
+    </ul>
+</nav>
+
+
+
+
+
+
+
+
+
+
+<?php
+include(VIEWS . "./inc/footer.php");
